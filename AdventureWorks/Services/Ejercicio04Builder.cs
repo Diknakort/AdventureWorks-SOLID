da742396b15c90180981f8972f31f40425699ebf
Diknakort <diknakort@gmail.com>
2024-05-29 13:37:23 +0200

    Agregar archivos de proyecto.

---
+++ AdventureWorks/Services/Ejercicio04Builder.cs
@@ -0,0 +1,39 @@
+﻿using AdventureWorks.Models;
+
+namespace AdventureWorks.Services
+{
+    public class Ejercicio04Builder : IProductSpecification, IProductoQuery
+    {
+        private IProductSpecification especificacion;
+
+        public Ejercicio04Builder()
+        {
+            IProductSpecification PorInicio = new NameComienzaSpecification()
+            {
+                letras = ["A","B","C"]
+            };
+            IProductSpecification PorColor = new ProductColorSpecification()
+            {
+                colores = ["RED", "BLACK", "BLUE"]
+            };
+            IProductSpecification PorPrecio = new ListPriceSpecification()
+            {
+                Price = 3
+            }; 
+            especificacion = new AndSpecification()
+            {
+                Spec1 = PorInicio,
+                Spec2 = PorColor,
+                Spec3 = PorPrecio
+            };
+        }
+        public IEnumerable<Product> dameProductos(IEnumerable<Product> products)
+        {
+            return products.Where(x => especificacion.isValid(x)).OrderBy(x => x.Name);
+        }
+        public bool isValid(Product _producto)
+        {
+            return especificacion.isValid(_producto);
+        }
+    }
+}
